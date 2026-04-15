a74fd72e3fb9420fdf52761381c4fdac1709fe67
Ankit-Basu <ankitbasu10@gmail.com>
2026-04-15 23:20:50 +0530

    initial maven project

---
+++ src/main/java/com/vikas/demo/App.java
@@ -0,0 +1,14 @@
+package com.vikas.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello, Devops World" );
+        System.out.println("This is our project");
+    }
+}
